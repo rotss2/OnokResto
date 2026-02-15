da5266221ed0d1a40b7cf20fdfb865720cefef49
rotss2 <ronnvincentpingol2005@gmail.com>
2026-02-15 15:07:52 +0800

    first-commit

--- backend/views.py
+++ backend/views.py
@@ -1,5 +1,13 @@
+from flask import Blueprint
+from __init__ import db
+
+views = Blueprint('views', __name__)
+
+@views.route('/')
+def home():
+    return "Welcome to OnokResto!"
+
 @views.route('/init-db')
 def init_db():
-    from __init__ import db
     db.create_all()
     return "Database tables created!"
